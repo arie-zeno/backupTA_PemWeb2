517d7c9a52f772297ae4009ef47784329e5dc24e
arie-zeno <ariyono281201@gmail.com>
2023-09-20 10:39:15 +0800

    update ui map

--- resources/views/lacak.blade.php
+++ resources/views/lacak.blade.php
@@ -1,5 +1,6 @@
 
 @extends("layouts.main")
+
 <style>
 
     span{
@@ -30,54 +31,62 @@
     }
     .label-kecamatan{
         color: black;
+        /* text-shadow: 1px 1px white; */
         text-align: center;
         font-weight: bold;
     }
 </style>
 
-  <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
-  integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
-  crossorigin=""/>
-
-  <!-- Make sure you put this AFTER Leaflet's CSS -->
-  <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
-    integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
-    crossorigin=""></script>
-
-    {{-- Plugin Path --}}
-    {{-- <link rel="stylesheet" href="/css/leaflet-measure-path.css">
-    <script src="/js/leaflet-measure-path.js"></script> --}}
-    {{-- <script src="https://elfalem.github.io/Leaflet.curve/src/leaflet.curve.js"></script> --}}
-
-    {{-- polyline --}}
-    {{-- <link rel="stylesheet" href="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.css" />
-    <script src="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.js"></script> --}}
-
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
-    {{-- routing machine --}}
-    <link rel="stylesheet" href="/js/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
-    <script src="/js/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
-
-    {{-- Slide --}}
-    <link rel="stylesheet" href="/css/L.Control.SlideMenu.css">
-    <script src="/js/L.Control.SlideMenu.js"></script>
-
-    {{-- cluster --}}
-    <link rel="stylesheet" href="js/Leaflet.markercluster/dist/MarkerCluster.css" />
-	<link rel="stylesheet" href="/js/Leaflet.markercluster/dist/MarkerCluster.Default.css" />
-	<script src="/js/Leaflet.markercluster/dist/leaflet.markercluster-src.js"></script>
-  <link rel="stylesheet" href="/js/treeLayer/L.Control.Layers.Tree.css">
-  <script src="/js/treeLayer/L.Control.Layers.Tree.js"></script>
+  
 @section("container")
+
   <div class="container-fluid px-5">
         <div class="row justify-content-center align-items-center" style="height: 100vh" id="about">
             <div class="col-sm-12">
-                <h2 class="fw-bold">Persebaran Alumni</h2>
+                {{-- <h2 class="fw-bold">Persebaran Alumni</h2> --}}
                 <div id="map"></div>
           </div>
         </div>
       </div>
-      <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+            <link rel="stylesheet" href="https://unpkg.com/leaflet@1.9.4/dist/leaflet.css"
+        integrity="sha256-p4NxAoJBhIIN+hmNHrzRCf9tD/miZyoHS5obTRR9BMY="
+        crossorigin=""/>
+        
+
+        <!-- Make sure you put this AFTER Leaflet's CSS -->
+        <script src="https://unpkg.com/leaflet@1.9.4/dist/leaflet.js"
+            integrity="sha256-20nQCchB9co0qIjJZRGuk2/Z9VM+kNiyxNV1lvTlZBo="
+            crossorigin=""></script>
+
+            {{-- Plugin Path --}}
+            {{-- <link rel="stylesheet" href="/css/leaflet-measure-path.css">
+            <script src="/js/leaflet-measure-path.js"></script> --}}
+            {{-- <script src="https://elfalem.github.io/Leaflet.curve/src/leaflet.curve.js"></script> --}}
+
+            {{-- polyline --}}
+            {{-- <link rel="stylesheet" href="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.css" />
+            <script src="https://ppete2.github.io/Leaflet.PolylineMeasure/Leaflet.PolylineMeasure.js"></script> --}}
+
+            
+            <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+            
+            {{-- cluster --}}
+            <link rel="stylesheet" href="js/Leaflet.markercluster/dist/MarkerCluster.css" />
+            <link rel="stylesheet" href="/js/Leaflet.markercluster/dist/MarkerCluster.Default.css" />
+            <script src="/js/Leaflet.markercluster/dist/leaflet.markercluster-src.js"></script>
+            <link rel="stylesheet" href="/js/treeLayer/L.Control.Layers.Tree.css">
+            <script src="/js/treeLayer/L.Control.Layers.Tree.js"></script>
+            {{-- Slide --}}
+            <link rel="stylesheet" href="/css/L.Control.SlideMenu.css">
+            <script src="/js/L.Control.SlideMenu.js"></script>
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.0/jquery.min.js"></script>
+              {{-- routing machine --}}
+              <link rel="stylesheet" href="/js/leaflet-routing-machine/dist/leaflet-routing-machine.css" />
+              <script src="/js/leaflet-routing-machine/dist/leaflet-routing-machine.js"></script>
+  
+              
+          
+      
       <script>
 
         let navbar = document.getElementById("navbar");
@@ -106,6 +115,16 @@
             iconSize:     [50, 50],
             });
 
+            var ceweIcon = L.icon({
+            iconUrl: "/img/icon_cewe.png",
+            iconSize:     [70, 70],
+            });
+
+            var cowoIcon = L.icon({
+            iconUrl: "/img/icon_cowo.png",
+            iconSize:     [70, 70],
+            });
+
             
 
 
@@ -117,7 +136,7 @@
         //     }).addTo(map);
 
             var baseLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-                  attribution: '© OpenStreetMap contributors'
+                  attribution: '© OpenStreetMap contributors',
               })
               baseLayer.addTo(map);
 
@@ -135,18 +154,14 @@
             @endphp
             var latitude = {{$koordinats[0]}},
                 longitude = {{$koordinats[1]}}
-                L.marker([latitude, longitude ], {icon:manIcon})
+                @if ($biodata["jk"] == "Laki-laki")
+                    icon = cowoIcon
+                @else
+                    icon = ceweIcon
+                @endif
+                L.marker([latitude, longitude ], {icon:icon})
                 .addTo(map)
-                .on("dblclick", (e) => {
-                        // var control = L.Routing.control({
-                        //     waypoints: [
-                        //         L.latLng(-3.298618801108944,114.58542404981114),
-                        //         L.latLng(latitude,longitude)
-                        //     ],
-                        //     routeWhileDragging: false
-                        // })
-                        // control.addTo(map);
-                })
+                
                 .bindPopup(
                     `Nama : {{$biodata["name"]}} <br>
                     NIM : {{$biodata["nim"]}} <br><br>
@@ -175,7 +190,7 @@
               }
 
             //   GeoJSON
-            var batasKecamatan = L.markerClusterGroup();
+            let batasKecamatan = [];
             let sub = [];
             let colors = [
               'rgba(255, 26, 104, 0.2)',
@@ -185,172 +200,128 @@
                     'rgb(153, 102, 255)',
                     'rgb(255, 159, 64)'
             ];
-            var tr = []
-            var baseTree = [
-                            {
-                                label: 'OpenStreeMap',
-                                layer: osm,
-                                children: [
-                                    {label: 'B&W', layer: osmBw, name: 'OpenStreeMap <b>B&W</b>'},
-                                    {label: 'OpenTopoMap', layer: otopomap, name: 'Topographic - OSM'},
-                                ]
-                            }
+            var kabupaten = []
+            var listKabupaten = []
+           var html = ``;
 
-                        ];
-            
-            $.getJSON('/geoJSON/banjarmasinPerKecamatan.geojson', (json) =>{
-                  let html = `
-                              <label id="banjar" onclick="showKecamatan(this)"><b> Kabuaten Banjarmasin </b></label> <br>
-                      `
-                      let i = 0;
-                  geoLayer = L.geoJSON(json, {
-                      
-                      style: (feature) => {
-                          return {
-                              fillOpacity: 0.8,
-                              weight: 5,
-                              opacity: 1,
-                              color: colors[i],
-                              borderColor: 'red'
-                            };
-                        },
-                        onEachFeature: (feature, layer)=>{
-                            var iconLabel = L.divIcon({
-                                className: 'label-kecamatan',
-                                html: `${feature.properties.KECAMATAN}`,
+           getShape("kabBanjarmasin", "Banjarmasin");
+           getShape("kabTapin", "Tapin");
+           getShape("kabBanjarbaru", "Banjarbaru");
+           getShape("kabBanjar", "Banjar");
+           getShape("kabBaritoKuala", "BaritoKuala");
+           getShape("kabHuluSungaiSelatan", "HuluSungaiSelatan");
+           getShape("kabHuluSungaiTengah", "HuluSungaiTengah");
+           getShape("kabHuluSungaiUtara", "HuluSungaiUtara");
+           getShape("kabBalangan", "Balangan");
+           getShape("kabTabalong", "Tabalong");
+           getShape("kabTanahLaut", "TanahLaut");
+           getShape("kabTanahBumbu", "TanahBumbu");
+           getShape("kabKotabaru", "Kotabaru");
 
-                            });
-                        html = html + `
-                        <input id="${i}" type="checkbox" class="kec" onclick="showBatas(this, ${i})">  <label for="${i}">${feature.properties.KECAMATAN} </label> <br>
-                        `
-                         sub.push(L.markerClusterGroup().addLayer(layer) )      
-                         tr.push({label: `${feature.properties.KECAMATAN}`, layer:layer});      
-                         
-                         L.marker(layer.getBounds().getCenter(), {icon:iconLabel}).addTo(sub[i]);
-                         i++;
-                         
-                            layer.addTo(batasKecamatan);
-                            // layer.addTo(i);
+            function getShape(namaFile, kab){
+
+                $.getJSON('/geoJSON/'+namaFile+'.geojson', (json) =>{
+                    html = html + `
+                                <label for="${kab}" style="cursor:pointer;" class="fs-6"><b> Kabupaten ${kab} <span id="label${kab}" class="fa fa-chevron-left"></span></b></label>
+                                <input id="${kab}" style="transform:scale(0)"  type="checkbox"  onclick="showKecamatan(this, ${batasKecamatan.length})">
+                                <br>
+                        `;
+                        let i = 0;
+                        let j = 1;
+                    geoLayer = L.geoJSON(json, {
+                        
+                        style: (feature) => {
+                            return {
+                                fillOpacity: 0.8,
+                                weight: 3,
+                                opacity: 1,
+                                color: 'purple',
+                                fillColor: colors[i]
+                                };
+                            },
+                            onEachFeature: (feature, layer)=>{
+                                var iconLabel = L.divIcon({
+                                    className: 'label-kecamatan',
+                                    html: `${feature.properties.WADMKC}`
+
+                                });
+                                console.log(feature.properties.WADMKC)
+                            // if(feature.properties.WADMKC){
+
+                                html = html + `
+                                <div class="${kab}" style="display:none">
+                                <input id="${sub.length}" type="checkbox" class="kec" onclick="showBatas(this, ${sub.length})">  <label class="text-capitalize" for="${sub.length}">${feature.properties.WADMKC}</label> <br>
+                            </div>
+                            `;
+                            
+                            // console.log(html)
+                            sub.push(L.markerClusterGroup().addLayer(layer) ) ;
+                            L.marker(layer.getBounds().getCenter(), {icon:iconLabel}).addTo(sub[batasKecamatan.length]);
+                            
+                            // batasKecamatan.addLayer(sub[i]);
+                            //  sub[i].addTo(batasKecamatan);
                             // batasKecamatan.addLayer(layer);
+                            batasKecamatan.push(L.markerClusterGroup().addLayer(sub[batasKecamatan.length]) ) ;
+                            i++;
+                        // }
+                            
                         }
                     })
-                    
-                    L.control.slideMenu(html).addTo(map);
-
-                    var overlaysTree = {
-                        label: ' Kota Lainnya',
-                        selectAllCheckbox: 'Un/select all',
-                        children: [
-                            {label: '<div id="onlysel">-Lihat yang diklik saja-</div>'},
-                            {label: 'Banjarmasin', children: tr}
-                            
-                            
-                        ]
+                    // console.log(batasKecamatan.length)
+                    for(let i = 0; i < batasKecamatan.length; i++){
+                        kabupaten.push(L.markerClusterGroup().addLayer(batasKecamatan[i]))
                     }
-                    var lay = L.control.layers.tree(baseTree, overlaysTree,
-                        {
-                            namedToggle: true,
-                            selectorBack: false,
-                            closedSymbol: '&#8862; &#x1f5c0;',
-                            openedSymbol: '&#8863; &#x1f5c1;',
-                            collapseAll: 'Collapse all',
-                            expandAll: 'Expand all',
-                            collapsed: false,
-                        });
-
-                    lay.addTo(map).collapseTree().expandSelected().collapseTree(true);
-                    L.DomEvent.on(L.DomUtil.get('onlysel'), 'click', function() {
-                        lay.collapseTree(true).expandSelected(true);
-                    });
+                    control2.setContents(html);
                 })
-       
-console.log(tr) 
-                var center = [40, 0];
+            }
+                var control2 = L.control.slideMenu("", {
+                    position: "topleft",
+                    menuposition: "topleft",
 
+                    }).addTo(map)
 
-var osm = L.tileLayer(
-    '//{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
-    {attribution: '© OpenStreetMap contributors'}
-);
-
-var osmBw = L.tileLayer(
-    'http://{s}.tiles.wmflabs.org/bw-mapnik/{z}/{x}/{y}.png',
-    {attribution: '© OpenStreetMap contributors'}
-);
-
-var otopomap = L.tileLayer(
-    '//{s}.tile.opentopomap.org/{z}/{x}/{y}.png',
-    {attribution: '© OpenStreetMap contributors. OpenTopoMap.org'}
-);
-
-var thunderAttr = {attribution: '© OpenStreetMap contributors. Tiles courtesy of Andy Allan'}
-var transport = L.tileLayer(
-    '//{s}.tile.thunderforest.com/transport/{z}/{x}/{y}.png',
-    thunderAttr
-);
-
-var cycle = L.tileLayer(
-    '//{s}.tile.thunderforest.com/cycle/{z}/{x}/{y}.png',
-    thunderAttr
-);
-
-
-
-
-
-
-// var overlaysTree = {
-//     label: 'Some cities',
-//     selectAllCheckbox: 'Un/select all',
-//     children: [
-//         {label: '<div id="onlysel">-Show only selected-</div>'},
-//         {label: 'Banjarmasin', children: [
-//             {label: 'Paris', layer: L.marker(sub[1].getBounds().getCenter() )},
-//             {label: 'Toulouse', layer: L.marker([43.629, 1.364])},
-//         ]},
-//         {label: 'Germany', selectAllCheckbox: true, children: [
-//             {label: 'Berlin', layer: L.marker([52.559, 13.287])},
-//             {label: 'Cologne', layer: L.marker([50.866, 7.143])},
-//             {label: 'Hamburg', layer: L.marker([53.630, 9.988])},
-//             {label: 'Munich', layer: L.marker([48.354, 11.786])},
-//         ]},
-        
-//     ]
-// }
-
-
-// var baseLayers = {
-//     "base": baseLayer
-// };
-                
-// var overlays = {
-//     "Marker": geoJSON
-// };
-
-// L.control.layers(baseLayers, overlays).addTo(map);
-
-// html = html + `wkwk`
+                // L.control.slideMenu(html).addTo(map);
+          
 function showBatas(v, i){
     if(v.checked === true){
-        map.removeLayer(batasKecamatan);
+        // map.removeLayer(batasKecamatan);
         map.addLayer(sub[i]);
         map.flyTo(sub[i].getBounds().getCenter());
         
     }else{
         map.removeLayer(sub[i]);
+       
     }
 }
 
-// function showKecamatan(v){
-//     if(v.checked === true){
-//         map.addLayer(batasKecamatan);
-//     }else{
-//         map.removeLayer(batasKecamatan);
-//         v.checked = false;
-//     }
-// }
+function showKecamatan(v, i){
+    let inp = v.parentElement.querySelectorAll("."+v.id),
+        span = v.parentElement.querySelector("#label"+v.id);
+console.log(span)
+    
+    
+    if(v.checked === true){
+        // map.addLayer(batasKecamatan);
+        // var class = $(".Balangan");    
+            for(let i = 0; i < inp.length; i++){
+                inp[i].style.display = "";
+            }
+
+            span.className = "fa fa-chevron-down"
+
+
+        // map.flyTo(batasKecamatan[i].getBounds().getCenter());
+        
+        
+    }else{
+        for(let i = 0; i < inp.length; i++){
+            inp[i].style.display = "none";
+        }
+        // map.flyTo(batasKecamatan[i].getBounds().getCenter());
+    }
+}
 
       </script>
 <!-- After Leaflet script -->
-<script src="https://unpkg.com/leaflet.featuregroup.subgroup@1.0.2/dist/leaflet.featuregroup.subgroup.js"></script>
+{{-- <script src="https://unpkg.com/leaflet.featuregroup.subgroup@1.0.2/dist/leaflet.featuregroup.subgroup.js"></script> --}}
 @endsection
